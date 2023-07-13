e86ccebd4999ddc1cec568da56ee1e2a92be41cb
Noyon sarker <124178299+noyon674@users.noreply.github.com>
2023-07-13 12:56:09 +0600

    Create Advance.jsx

---
+++ src/ReactForm/Advance.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import style from './form.module.css';
+
+const Form = ()=>{
+
+    const [User, setUser] = useState({name:'', email:'', password:''});
+    const {name, email, password} = User;
+
+    const handleChange = (e)=>{
+        setUser({...User, [e.target.name]: e.target.value});
+    }
+
+    const submitHandler = (e)=>{
+        console.log(User)
+        e.preventDefault();
+    }
+    return(
+        <div>
+            <h1>Register Form</h1>
+            <form action="" onSubmit={submitHandler}>
+                <div>
+                    <label htmlFor="name">Name: </label>
+                    <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    onChange={handleChange}
+                    required/>
+                </div>
+                <div>
+                    <label htmlFor="email">Email: </label>
+                    <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    onChange={handleChange}
+                    required/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    onChange={handleChange}
+                    required/>
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
